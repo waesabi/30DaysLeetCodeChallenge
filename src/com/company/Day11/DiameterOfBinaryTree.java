5371c4c0dd9bbed234e8b9862b1f4bc6161b24c4
Sanket <sanket@mapprr.com>
2020-04-12 03:52:51 +0530

    Day10 - Diameter of binary tree  Ccmpleted  🚀

---
+++ src/com/company/Day11/DiameterOfBinaryTree.java
@@ -0,0 +1,25 @@
+package com.company.Day11;
+
+
+public class DiameterOfBinaryTree {
+
+    // Accepted
+    public static int max = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        if (root == null) { return 0;}
+        max = 0;
+        getDiameter(root);
+        return max - 1;
+    }
+
+    public static int getDiameter(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = getDiameter(root.left);
+        int right = getDiameter(root.right);
+        max = Math.max(max, left + right + 1);
+        return Math.max(left, right) + 1;
+    }
+
+}
---
+++ src/com/company/Day11/TreeNode.java
@@ -0,0 +1,8 @@
+package com.company.Day11;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}

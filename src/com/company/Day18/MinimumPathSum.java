eb363c0969e4a3e99a2e8c634e15de33631c6220
Sanket <sanket@mapprr.com>
2020-04-18 21:18:34 +0530

    Day18 completed 🚀

---
+++ src/com/company/Day18/MinimumPathSum.java
@@ -0,0 +1,34 @@
+package com.company.Day18;
+
+public class MinimumPathSum {
+
+    public static void main(String[] args) {
+        int[][] arr = {{1,3,1},{1,5,1},{4,2,1}};
+        minPathSum(arr);
+    }
+
+    // Accepted - Easy Dp Problem
+    public static int minPathSum(int[][] grid) {
+        int rows = grid.length;
+        if (rows < 1) { return 0; }
+        int cols = grid[0].length;
+        int[][] dp = new int[rows][cols];
+        dp[0][0] = grid[0][0];
+        // 0th row
+        for (int i = 1; i < cols; i++) {
+            dp[0][i] = dp[0][i-1] + grid[0][i];
+        }
+        // 0th col
+        for (int i = 1; i < rows; i++) {
+            dp[i][0] = dp[i-1][0] + grid[i][0];
+        }
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                dp[i][j] = grid[i][j] +
+                        Math.min(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return dp[rows - 1][cols - 1];
+    }
+
+}

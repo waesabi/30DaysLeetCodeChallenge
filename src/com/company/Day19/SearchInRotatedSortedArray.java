337ea2898b9910b1537e2fd354944897a476d8e9
Sanket <sanket@mapprr.com>
2020-04-19 21:07:28 +0530

    Day19 Completed , Added better approach

--- src/com/company/Day19/SearchInRotatedSortedArray.java
+++ src/com/company/Day19/SearchInRotatedSortedArray.java
@@ -2,56 +2,44 @@ package com.company.Day19;
 
 public class SearchInRotatedSortedArray {
 
-    public int search(int[] arr, int target) {
-        int index = findPivot(arr);
-        if (index == -1) {
-            return search(arr,target,0,arr.length - 1);
-        }
-        else if (arr[index] == target) {
-            return index;
-        }
-        else if (arr[0] > target) {
-            return search(arr,target,index+1, arr.length - 1);
-        }
-        else {
-            return search(arr,target,0, index - 1);
-        }
+    public static void main(String[] args) {
+        int[] arr = {4,5,6,7,0,1,2};
+        int target = 0;
+        System.out.println(search(arr,target));
     }
 
-    public static int findPivot(int[] arr) {
-        int left = 0, right = arr.length - 1;
+    /*
+        Read below blog for clear explanation
+        https://leetcode.com/problems/search-in-rotated-sorted-array/discuss/14436/Revised-Binary-Search
+     */
+    public static int search(int[] arr, int target) {
+        if (arr == null || arr.length == 0) {
+            return -1;
+        }
+        int size = arr.length;
+        int left = 0, right = size - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (mid - 1 >= 0 && arr[mid - 1] > arr[mid]) {
-                return mid;
-            }
+            int midValue = arr[mid];
+            if (midValue == target) { return mid; }
 
-            if (arr[mid] >= arr[0]) {
-                left = mid + 1;
+            if (midValue >= arr[left]) {
+                if (target >= arr[left] && target < midValue) {
+                    right = mid - 1;
+                }
+                else {
+                    left = mid + 1;
+                }
             }
             else {
-                right = mid - 1;
+                if (target > midValue && target <= arr[right]) {
+                    left = mid + 1;
+                }
+                else {
+                    right = mid - 1;
+                }
             }
-
         }
         return -1;
     }
-
-    public static int search(int[] nums, int target, int left,
-                             int right) {
-        if (left > right) {
-            return -1;
-        }
-        int mid = left + (right - left) / 2;
-        if (nums[mid] == target) {
-            return mid;
-        }
-        else if (nums[mid] > target) {
-            return search(nums, target, left,mid - 1);
-        }
-        else {
-            return search(nums,target,mid + 1, right);
-        }
-    }
-
 }
